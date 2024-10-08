4ff4adfeae2adc1d770ba4dbb00e16fcef36a892
rauanb <rauan_b@hotmail.com>
2024-10-08 19:40:38 -0300

    corrigida data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const eventDate = new Date("Dec 12, 2022 19:00:00");
+const eventDate = new Date("Dec 12, 2024 19:00:00");
 const eventTimeStamp = eventDate.getTime();
 
 const countDown = setInterval(function(){
